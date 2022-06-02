64e642dcebb9579f8c6bc2f842d600cc745a15b4
Sky Jones <rsajones94@gmail.com>
2022-06-02 11:10:53 -0500

    no message

---
+++ mr_processing_tools/pipeline/analyze/analyze_regionstats.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+help_info = """
+
+
+BOILERPLATE
+
+This script uses takes a T1-weighted MR image of the brain and calls FreeSurfer
+to segment its tissue types. It then uses the resulting data to make a simple
+tabulation of some key values.
+
+The script will create:
+    1) x
+        de
+    2) y
+        de
+
+input:
+    -t / --t1 : the path to the t1, which is a .nii.gz file.
+        This should be a raw T1, not a brain-extracted T1
+    -h / --help : brings up this helpful information. does not take an argument
+"""
+
+pass
